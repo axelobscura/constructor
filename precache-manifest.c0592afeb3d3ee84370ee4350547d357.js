29ec1f1375b1488b691ed0fd0e0be6e54a8b4575
Axel <axosar@gmail.com>
2020-02-12 12:24:03 -0600

    Updates

---
+++ precache-manifest.c0592afeb3d3ee84370ee4350547d357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0441688660338a53bb9a557d47d4fd32",
+    "url": "/www.constructor.com/app/index.html"
+  },
+  {
+    "revision": "23f50593e49c55b87cf5",
+    "url": "/www.constructor.com/app/static/css/main.9a476bf1.chunk.css"
+  },
+  {
+    "revision": "7a4aae81b09327f075ce",
+    "url": "/www.constructor.com/app/static/js/2.3fe2fa2d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/www.constructor.com/app/static/js/2.3fe2fa2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f50593e49c55b87cf5",
+    "url": "/www.constructor.com/app/static/js/main.1c4f047f.chunk.js"
+  },
+  {
+    "revision": "fe8260cbf6ff43b22989",
+    "url": "/www.constructor.com/app/static/js/runtime-main.76d63964.js"
+  },
+  {
+    "revision": "f09113e7f34cd5ccef0cd7a57ee4f83f",
+    "url": "/www.constructor.com/app/static/media/bkg.f09113e7.jpg"
+  },
+  {
+    "revision": "b883545a6ca067845a5dc2259da9c0e0",
+    "url": "/www.constructor.com/app/static/media/logo.b883545a.svg"
+  }
+]);
